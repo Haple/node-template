7e1361f8876add4fcfa7bc123eb0b403c40ea3cb
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-11-07 15:44:21 -0300

    Template inicial

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8409a4ba7fc71280de8858a3d7bcded0',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'UmaSenhaMuitoBoa',
+  database: 'node-template',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
